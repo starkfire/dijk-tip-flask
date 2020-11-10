c81f815fb971af85d7b61e51a4db4e15d2e80754
Pocholo <plpantoja49@gmail.com>
2020-11-11 00:28:18 +0800

    refactor: change pathfinder implementation

--- scripts/pathfinder.py
+++ scripts/pathfinder.py
@@ -1,24 +1,25 @@
 import sys
 import adjacency_matrix as matrix
 
-# initialize variables
-nodes = []                      # an array for representing each node
-vertices, edges = None, None    # vertices and edges of a graph
-n_vtx = 0                       # number of vertices
-inf = sys.maxsize               # signifies an infinite value
+inf = sys.maxsize
 
-def get_graph(adj_matrix):
+class Pathfinder:
     """
-    Returns the vertices and edges from an input adjacency matrix.
+    Dijkstra Pathfinder Implementation
     """
-    return adj_matrix.vertices, adj_matrix.edges
 
-def create_nodes(src_index, n_vtx):
-    """
-    Creates an array for representing each vertex as a node [x, y]:
-        x : indicates if vertex/node has been visited (1) or not (0)
-        y : indicates the distance of the node from the input source
-    """
-    for x in range(n_vtx):
-        nodes.append([0, 0]) if x == src_index else nodes.append([0, inf])
-    return None
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
+        self.n_vtx = len(vertices)
+        self.nodes = []
+    
+    def create_nodes(self, src):
+        """
+        Creates an array for representing each vertex as a node [x, y]:
+            x : indicates if vertex/node has been visited (1) or not (0)
+            y : indicates the distance of the node from the input source
+        """
+        for x in range(self.n_vtx):
+            self.nodes.append([0, 0]) if x == src else self.nodes.append([0, inf])
+        return None

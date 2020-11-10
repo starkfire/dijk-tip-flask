87ac811aa8f5eb492b9ac13c316421be0870d669
Pocholo <plpantoja49@gmail.com>
2020-11-11 00:32:13 +0800

    refactor: create function for getting adjacent nodes

--- scripts/pathfinder.py
+++ scripts/pathfinder.py
@@ -23,3 +23,16 @@ class Pathfinder:
         for x in range(self.n_vtx):
             self.nodes.append([0, 0]) if x == src else self.nodes.append([0, inf])
         return None
+    
+    def get_adjacent_nodes(self, node: int):
+        """
+        Returns the index of nodes that are adjacent to the input node.
+        """
+        adjacent_nodes = []
+        node_vtx = self.vertices[node]
+
+        for x in range(len(node_vtx)):
+            if node_vtx[x] == 1 and self.nodes[x][0] == 0:
+                adjacent_nodes.append(x)
+        
+        return adjacent_nodes

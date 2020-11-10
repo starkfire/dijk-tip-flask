1f3483fe72b06d6136bc3cd40eac97a5856a8a98
Pocholo <plpantoja49@gmail.com>
2020-11-11 00:39:45 +0800

    refactor: method for identifying the next traversable node

--- scripts/pathfinder.py
+++ scripts/pathfinder.py
@@ -36,3 +36,38 @@ class Pathfinder:
                 adjacent_nodes.append(x)
         
         return adjacent_nodes
+    
+    def get_next_node(self, node: int):
+        """
+        Returns the nearest, unvisited node that is adjacent to the input node.
+        """
+        # get adjacent nodes
+        adjacent_nodes = self.get_adjacent_nodes(node)
+        
+        # it's either our script failed to traverse all nodes or it has reached all nodes
+        # either way, issue a warning
+        if len(adjacent_nodes) == 0:
+            print("WARNING: End of the Line")
+            return None
+        
+        for x in range(len(adjacent_nodes)):
+            # if there is only a single adjacent node left, there's no need to loop further
+            if len(adjacent_nodes) == 1: break
+            
+            # index of the adjacent node, index of the other adjacent node
+            adj, other_adj = adjacent_nodes[x], adjacent_nodes[x - 1]
+
+            # compare the distance of the adjacent node to the other adjacent node
+            if (self.edges[node][adj] <= self.edges[node][other_adj]):
+                adjacent_nodes.pop(x-1)
+        
+        return adjacent_nodes[0]
+    
+    def dijkstra(self, src, dest):
+        """
+        Returns the shortest path from the input source node to destination node
+        """
+        # convert the input parameters to interpretable index
+        src, dest = ord(src) - 97, ord(dest) - 97
+        # create an array to represent each vertex as a node
+        self.create_nodes(src)
